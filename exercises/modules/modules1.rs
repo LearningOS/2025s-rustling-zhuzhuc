cab96aad4c46edd0baa33b9b63db9a161e3a3bb4
zhuzhuc <1727643988@qq.com>
2025-03-20 10:00:38 +0800

    update

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,20 +3,23 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 mod sausage_factory {
-    // Don't let anybody outside of this module see this!
+    // 私有函数，外部模块无法访问
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
+    // bug修复：添加 pub 关键字，让外部模块可以访问该函数
+    pub fn make_sausage() {
+        let recipe = get_secret_recipe();
+        println!("使用秘方: {} 制作香肠", recipe);
         println!("sausage!");
     }
 }
 
 fn main() {
+    // 调用香肠工厂模块的制作香肠函数
     sausage_factory::make_sausage();
 }
--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,12 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // bug修复：添加 pub 关键字，将导入的常量重新导出
+    pub use crate::delicious_snacks::fruits::PEAR as fruit;
+    pub use crate::delicious_snacks::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
