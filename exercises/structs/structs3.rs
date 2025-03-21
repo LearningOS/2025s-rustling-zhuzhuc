8d590f19e66e5fc71afbdef473cb8cfbaea6936b
zhuzhuc <1727643988@qq.com>
2025-03-21 23:36:18 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -27,11 +27,13 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> bool {
+    fn is_international(&self) -> bool{
+        // Something goes here...
         self.sender_country != self.recipient_country
     }
 
     fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        // Something goes here...
         self.weight_in_grams * cents_per_gram
     }
 }
